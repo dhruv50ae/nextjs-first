a82bc964d1fead3ac23f045ff12c5adb21eb2966
dhruv50ae <linkdp2012@gmail.com>
2022-06-13 20:18:44 +0100

    Dynamic links
    For dynamic links you can use a link with href set to /clients/nameOfId with content

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -2,9 +2,20 @@ import React from "react";
 import Link from "next/link";
 
 const ClientsPage = () => {
+  const clients = [
+    { id: "Dhruv50AE", name: "Dhruv.50AE" },
+    { id: "Dhruv49AE", name: "Dhruv.549AE" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>;
+        })}
+      </ul>
     </div>
   );
 };
