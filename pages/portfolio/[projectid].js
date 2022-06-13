6b2cf0a7c2245faa14e51ebb9484dfb0b9cf2cb3
dhruv50ae <linkdp2012@gmail.com>
2022-06-13 16:13:19 +0100

    UseRouter Hook
    the use router hook allows you to use the two methods of pathname and query to navigate to the square brackets in the file name

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,6 +1,11 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const PortfolioProjectPage = () => {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
   return <div>The Portfolio Project Page</div>;
 };
 
