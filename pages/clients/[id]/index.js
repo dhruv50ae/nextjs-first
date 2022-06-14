3f2aa088091157ad7964e63e5c36b11d2031eb02
dhruv50ae <linkdp2012@gmail.com>
2022-06-14 13:33:34 +0100

    pushing pages
    by using router.push, you can navigate to pages by passing in something like /clients/Dhruv50AE/projecta. This can be replaced by router.replace which means you cannot go back to the page once you clicked the button. Alternatively you can pass in an object with pathname /clients/[id]/[clientprojectid] then a query set to an object of id: Dhruv50AE and clientprojectid of projecta

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,17 @@ import { useRouter } from "next/dist/client/router";
 
 const ClientProjectsPage = () => {
   const router = useRouter();
+  const loadProjectHandler = () => {
+    // Load data
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  };
   return (
     <div>
       <h1>The Client Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
